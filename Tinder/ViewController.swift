2a065039f318f7c3dae383cf2a33daf6653ccc57
Iaroslav Krasnokutskii <Yroslavk1995@gmail.com>
2022-01-11 13:08:40 +0300

    StackView doesn't work properly

--- Tinder/ViewController.swift
+++ Tinder/ViewController.swift
@@ -12,6 +12,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let view1 = UIView()
+        view1.backgroundColor = .yellow
+        
+        let view2 = UIView()
+        view1.backgroundColor = .blue
+        
+        let view3 = UIView()
+        view1.backgroundColor = .green
+     
+        let stackView = UIStackView(arrangedSubviews: [view3, view2, view1])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        
+        view.addSubview(stackView)
+        
+        [stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+         stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+         stackView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+         stackView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)].forEach{ $0.isActive = true}
+
     }
 
 
